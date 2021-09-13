ea91ebbf2e61ad6541f1e984afd9b1ee493d1c66
ThiagoNeckel <64933079+ThiagoNeckel@users.noreply.github.com>
2021-09-13 12:43:08 -0300

    commit sem net2

--- GrupoQuatro/src/entidades/Compra.java
+++ GrupoQuatro/src/entidades/Compra.java
@@ -1,134 +1,81 @@
-package entidadeDao;
+package entidades;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import entidades.Compra;
-import entidades.Pessoa;
-import util.ConnectionFactory;
+public class Compra extends Estoque{
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-public class CompraDao {
-
-	private Connection con = null;
-
-	public CompraDao() {
-		con = (ConnectionFactory.getConnection());
-	}
-
-
-	public boolean inserir(Compra compra) {
-		 
-		String sql = "INSERT INTO compra (pessoa, estoques, quantidade, valor) VALUES (?,?,?,?)";
-		PreparedStatement stmt = null;
-
-		try {
-			stmt = con.prepareStatement(sql);
-			stmt.setInt(1, compra.getPessoa().getId());
-			stmt.setInt(2, compra.getEstoque().getProduto().getId());
-			stmt.setInt(3, compra.getQuantidade());
-			stmt.setDouble(4, compra.getValor());
-			stmt.executeUpdate();
-			stmt.close();
-			con.close();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("erro ao Inserir" + e);
-			return false;
-		}
+	private int id;
+	private String data;
+	private Pessoa pessoa;
+	private Estoque estoque;
+	private double valor;
+	public Compra() {
 
 	}
 
-	
-	public List<Compra> Buscartodos() {
-
-		String sql = "SELECT * FROM compraestoque";
-
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-
-		List<Compra> compras = new ArrayList<Compra>();
-
-		try {
-			stmt = con.prepareStatement(sql);
-			rs = stmt.executeQuery();
-
-			while (rs.next()) {
-				Compra compra = new Compra();
-				compra.getPessoa().setNome(rs.getString("nome"));
-				compra.getPessoa().setIndentificacao(rs.getString("identificacao"));
-				compra.getPessoa().setTipoPessoa(rs.getString("tipoPessoa"));
-				compra.setData(rs.getString("data"));
-				;
-				compra.getEstoque().setQuantidade(rs.getInt("quantidade"));
-				compra.getEstoque().setValor(rs.getDouble("valor"));
-
-				compras.add(compra);
-			}
-			stmt.close();
-			rs.close();
-			con.close();
-
-		} catch (SQLException e) {
-			System.out.println("erro" + e);
-		}
-		return compras;
-	}
-
-	
-	public boolean atualizar(Compra compra) {
+	public Compra(int id, String data, Pessoa pessoa, Estoque estoque, double valor) {
 		
-		String sql = "UPDATE compra SET pessoa = ? , data = ? , quantidade = ? , valor=?  estoque = ? WHERE id = (?,?,?,?,?)";
-		PreparedStatement stmt = null;
-
-		try {
-			stmt = con.prepareStatement(sql);
-			stmt.setInt(1, compra.getPessoa().getId());
-			stmt.setString(2,compra.getData());
-			stmt.setInt(3, compra.getEstoque().getProduto().getId());
-			stmt.setInt(4, compra.getId());
-			stmt.setInt(5, compra.getQuantidade());
-			stmt.setDouble(6, compra.getValor());
-			stmt.executeUpdate();
-			stmt.close();
-			con.close();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("erro ao Atualizar" + e);
-			return false;
-		}
-
+		this.id = id;
+		this.data = data;
+		this.pessoa = pessoa;
+		this.estoque = estoque;
+		this.valor = valor;
 	}
 
-	
-	public boolean delete(Compra compra) {
-		
-		String sql = "DELETE FROM marca WHERE id = ?";
-		PreparedStatement stmt = null;
-
-		try {
-			stmt = con.prepareStatement(sql);
-			stmt.setInt(1, compra.getId());
-			stmt.executeUpdate();
-			stmt.close();
-			con.close();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("erro ao Deletar" + e);
-			return false;
-		}
-
+	public int getId() {
+		return id;
 	}
 
-	
-	public long proximoCodigo() {
-		List<Pessoa> compras = new ArrayList<Pessoa>();
+	public void setId(int id) {
+		this.id = id;
+	}
 
-		return compras.size() + 1;
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	
+
+	public Estoque getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Estoque estoque) {
+		this.estoque = estoque;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public void entrada(int quantidade) {
+		quantidade += quantidade;
+		super.saida(quantidade);
+	}
+
+	@Override
+	public String toString() {
+
+		return getProduto().toString() + sdf.format(data);
 	}
 
 }
