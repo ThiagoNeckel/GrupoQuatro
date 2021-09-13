a5d9abef755e1625d916aa385a61587040cf7256
ThiagoNeckel <64933079+ThiagoNeckel@users.noreply.github.com>
2021-09-13 12:46:33 -0300

    sem net

--- GrupoQuatro/src/entidades/Venda.java
+++ GrupoQuatro/src/entidades/Venda.java
@@ -1,33 +1,82 @@
 package entidades;
 
-/**
- *
- * @author Celiculos
- */
-public class Venda extends MovimentacaoBase {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public Venda(final long numero) {
-        super(numero);
-    }
+public class Venda extends Estoque {
 
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-    @Override
-    protected ItemMovimentacao criaItem(Produto produto, double quantidade) {
-        return new ItemVenda(
-            (
-                new dao.ItemVendaDao(
-                    new ItemVenda(0, this, null, 0)
-                ).proximoCodigo()
-            ),
-            this,
-            produto,
-            quantidade
-        );
-    }
+	private int id;
+	private String data;
+	private Pessoa pessoa;
+	private Estoque estoque;
+	private double valor;
+	public Venda() {
 
-    @Override
-    protected void salva() {
-        (new dao.VendaDao(this)).insere();
-    }
+	}
+
+	public Venda(int id, String data, Pessoa pessoa, Estoque estoque, double valor) {
+		
+		this.id = id;
+		this.data = data;
+		this.pessoa = pessoa;
+		this.estoque = estoque;
+		this.valor = valor;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	
+
+	public Estoque getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Estoque estoque) {
+		this.estoque = estoque;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public void saida(int quantidade) {
+		quantidade -= quantidade;
+		super.saida(quantidade);
+	}
+
+	@Override
+	public String toString() {
+
+		return getProduto().toString() + sdf.format(data);
+	}
 
 }
