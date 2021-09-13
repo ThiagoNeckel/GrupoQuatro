537eb8eaa0c5986cb29516924d781779e4346374
ThiagoNeckel <64933079+ThiagoNeckel@users.noreply.github.com>
2021-09-13 12:42:16 -0300

    commit sem net

--- GrupoQuatro/src/entidades/Compra.java
+++ GrupoQuatro/src/entidades/Compra.java
@@ -1,51 +1,134 @@
-package entidades;
+package entidadeDao;
 
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Movimentação de Entrada.
- *
- * @author Celiculos
- */
-public class Compra extends MovimentacaoBase {
+import entidades.Compra;
+import entidades.Pessoa;
+import util.ConnectionFactory;
 
-    private final String fornecedor;
+public class CompraDao {
 
+	private Connection con = null;
 
-    public Compra(final long numero, final String fornecedor) {
-        super(numero);
-        this.fornecedor = fornecedor;
-    }
+	public CompraDao() {
+		con = (ConnectionFactory.getConnection());
+	}
 
 
-    public String getFornecedor() {
-        return this.fornecedor;
-    }
+	public boolean inserir(Compra compra) {
+		 
+		String sql = "INSERT INTO compra (pessoa, estoques, quantidade, valor) VALUES (?,?,?,?)";
+		PreparedStatement stmt = null;
 
-    @Override
-    protected ItemMovimentacao criaItem(Produto produto, double quantidade) {
-        return new ItemCompra(
-            (
-                new dao.ItemCompraDao(
-                    new ItemCompra(0, this, null, 0)
-                ).proximoCodigo()
-            ),
-            this,
-            produto,
-            quantidade
-        );
-    }
+		try {
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, compra.getPessoa().getId());
+			stmt.setInt(2, compra.getEstoque().getProduto().getId());
+			stmt.setInt(3, compra.getQuantidade());
+			stmt.setDouble(4, compra.getValor());
+			stmt.executeUpdate();
+			stmt.close();
+			con.close();
+			return true;
+		} catch (SQLException e) {
+			System.out.println("erro ao Inserir" + e);
+			return false;
+		}
 
-    @Override
-    protected Map mapForBox() {
-        Map map = new java.util.HashMap<String, String>(super.mapForBox());
-        map.put("Fornecedor", this.getFornecedor());
-        return map;
-    }
+	}
 
-    @Override
-    protected void salva() {
-        (new dao.CompraDao(this)).insere();
-    }
+	
+	public List<Compra> Buscartodos() {
+
+		String sql = "SELECT * FROM compraestoque";
+
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		List<Compra> compras = new ArrayList<Compra>();
+
+		try {
+			stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				Compra compra = new Compra();
+				compra.getPessoa().setNome(rs.getString("nome"));
+				compra.getPessoa().setIndentificacao(rs.getString("identificacao"));
+				compra.getPessoa().setTipoPessoa(rs.getString("tipoPessoa"));
+				compra.setData(rs.getString("data"));
+				;
+				compra.getEstoque().setQuantidade(rs.getInt("quantidade"));
+				compra.getEstoque().setValor(rs.getDouble("valor"));
+
+				compras.add(compra);
+			}
+			stmt.close();
+			rs.close();
+			con.close();
+
+		} catch (SQLException e) {
+			System.out.println("erro" + e);
+		}
+		return compras;
+	}
+
+	
+	public boolean atualizar(Compra compra) {
+		
+		String sql = "UPDATE compra SET pessoa = ? , data = ? , quantidade = ? , valor=?  estoque = ? WHERE id = (?,?,?,?,?)";
+		PreparedStatement stmt = null;
+
+		try {
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, compra.getPessoa().getId());
+			stmt.setString(2,compra.getData());
+			stmt.setInt(3, compra.getEstoque().getProduto().getId());
+			stmt.setInt(4, compra.getId());
+			stmt.setInt(5, compra.getQuantidade());
+			stmt.setDouble(6, compra.getValor());
+			stmt.executeUpdate();
+			stmt.close();
+			con.close();
+			return true;
+		} catch (SQLException e) {
+			System.out.println("erro ao Atualizar" + e);
+			return false;
+		}
+
+	}
+
+	
+	public boolean delete(Compra compra) {
+		
+		String sql = "DELETE FROM marca WHERE id = ?";
+		PreparedStatement stmt = null;
+
+		try {
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, compra.getId());
+			stmt.executeUpdate();
+			stmt.close();
+			con.close();
+			return true;
+		} catch (SQLException e) {
+			System.out.println("erro ao Deletar" + e);
+			return false;
+		}
+
+	}
+
+	
+	public long proximoCodigo() {
+		List<Pessoa> compras = new ArrayList<Pessoa>();
+
+		return compras.size() + 1;
+	}
 
 }
